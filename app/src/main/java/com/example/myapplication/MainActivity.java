39d6affebaa48d7540679855cd9c123861e7b691
Bakyt <djumabaevb@gmail.com>
2021-03-22 16:50:47 +0600

    getting further

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
 import com.example.myapplication.model.Question;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
 
     private final Question[] questionBank = new Question[]{
             new Question(R.string.question_declaration, true),
@@ -18,6 +25,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResId());
+        binding.nextButton.setOnClickListener(view -> {
+            /*  Log.d("Main", "Watching onCreate" + questionBank[currentQuestionIndex++].getAnswerResId());*/
+            currentQuestionIndex = (currentQuestionIndex+1) % questionBank.length;
+            updateQuestion();
+        });
+        binding.prevButton.setOnClickListener(view -> {
+            if(currentQuestionIndex > 0) {
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
     }
 
+    private void updateQuestion() {
+        binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResId());
+    }
+    private void checkAnswer(boolean userChoseCorrect) {
+        boolean answerIsCorrect = questionBank[currentQuestionIndex].isAnswerTrue();
+
+    }
 }
